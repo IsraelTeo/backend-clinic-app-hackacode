94056fb707f20e38e60b87e1af06cda855a77538
Israel <israel.bastion@gmail.com>
2025-01-26 06:24:51 -0500

    database connection added

---
+++ db/connection.go
@@ -0,0 +1,38 @@
+package db
+
+import (
+	"fmt"
+
+	"gihub.com/IsraelTeo/clinic-backend-hackacode-app/config"
+	"gihub.com/IsraelTeo/clinic-backend-hackacode-app/model"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+//var GDB *gorm.DB
+
+func Connection(cfg *config.Config) (*gorm.DB, error) {
+	DSN := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		cfg.DBUser,
+		cfg.DBPassword,
+		cfg.DBHost,
+		cfg.DBPort,
+		cfg.DBName)
+
+	GDB, err := gorm.Open(mysql.Open(DSN), &gorm.Config{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return GDB, nil
+}
+
+func MigrateDB(GDB *gorm.DB) error {
+	err := GDB.AutoMigrate(&model.Service{})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"gihub.com/IsraelTeo/clinic-backend-hackacode-app/config"
+	"gihub.com/IsraelTeo/clinic-backend-hackacode-app/db"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	// Cargar las variables de entorno**
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Inicializar la configuración cargando las variables de entorno
+	cfg := config.InitConfig()
+
+	// Conectar a la base de datos utilizando la configuración cargada
+	gdb, err := db.Connection(cfg)
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+	}
+	fmt.Println("Database connection established successfully!")
+
+	// Migración de entidades
+	if err := db.MigrateDB(gdb); err != nil {
+		log.Fatalf("Error migrating database: %v", err)
+	}
+	fmt.Println("Database migration successful")
+
+	// Inicializar servidor Echo
+	//e := echo.New()
+
 }
