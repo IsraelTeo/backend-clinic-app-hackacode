3f528420f086c895d108ee4cecff609c24fad8f5
Israel <israel.bastion@gmail.com>
2025-01-27 18:34:31 -0500

    Package model added

--- model/service.go
+++ model/service.go
@@ -1,11 +1,23 @@
 package model
 
-import "time"
-
 type Service struct {
-	ID          uint      `gorm:"primarykey" json:"id"`
-	CreatedAt   time.Time `json:"-"`
-	Name        string    `json:"name" validate:"required,max=50"`
-	Description string    `json:"description" validate:"required,max=250"`
-	Price       float64   `json:"price" validate:"min=0,numeric"`
+	ID          uint    `gorm:"primaryKey;autoIncrement" json:"-"`
+	Name        string  `gorm:"size:50;not null" json:"name" validate:"required,max=50"`
+	Description string  `json:"description" validate:"required,max=250"`
+	Price       float64 `json:"price" validate:"min=0,numeric"`
+}
+
+type Package struct {
+	ID       uint      `gorm:"primaryKey;autoIncrement" json:"-"`
+	Name     string    `gorm:"size:50;not null" json:"name" validate:"required,max=50"`
+	Services []Service `gorm:"many2many:package_services;" json:"services"`
+	Price    float64   `json:"-"`
+}
+
+func (p *Package) CalculatePrice() {
+	total := 0.0
+	for _, service := range p.Services {
+		total += service.Price
+	}
+	p.Price = total
 }
