9ad618555e24d2239e64b5bd23758f2a368feecd
Israel <israel.bastion@gmail.com>
2025-02-25 14:39:14 -0500

    cors changes

--- config/cors.go
+++ config/cors.go
@@ -4,7 +4,7 @@ import "github.com/labstack/echo/v4/middleware"
 
 func CorsConfig() middleware.CORSConfig {
 	return middleware.CORSConfig{
-		AllowOrigins:     []string{"*"},
+		AllowOrigins:     []string{"https://clinic-administrator.vercel.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length", "Content-Disposition"},
